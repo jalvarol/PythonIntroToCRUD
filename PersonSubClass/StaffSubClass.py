fe0032cb9773bbd7771236240243cce169ef3eb2
Jose Leos <55753955+jalvarol@users.noreply.github.com>
2024-07-15 16:46:38 -0700

    Add files via upload

---
+++ PersonSubClass/StaffSubClass.py
@@ -0,0 +1,91 @@
+#This class contains the Staff Subclass - extends Person SuperClass
+
+
+import time
+import datetime
+import os
+from os import path
+from SuperPerson import Person
+
+class Staff(Person):
+    '''
+    This subclass Staff is-a Person. This subclass will change the tax rate
+    and a little line of code for the output file
+    '''
+    def __init__(self):
+        super().__init__()
+        self._tax = 0.09
+    '''
+    We override the method from Person since we know this class is a Staff,
+    therefore the tax variable is already set to 0.09
+    '''
+    def saveToFile(self):
+        '''
+        This part basically copies the printBill function but instead of printing
+        we will be writing to an outfile.It will include an identical copy along with
+        time stamps; small change - output file will state "Staff Bill"
+        '''
+        Flag1 = True
+        while Flag1:
+            if self._total == 0:
+                print("\nThank you! Take Care!")
+                Flag1 = False
+            else:
+                Directory = os.getcwd()
+                if path.isdir("Staff Receipts"):
+                     print("Receipt Added to Staff Receipt Folder")
+                else:
+                     os.chdir(Directory)
+                     os.mkdir("Staff Receipts")
+                Directory = os.path.join(Directory,"Staff Receipts")
+                timeStamp = time.time()
+                orderTimeStamp = datetime.datetime.fromtimestamp(timeStamp).strftime('%Y-%m-%d %H-%M-%S')
+                orderTimeStamp = orderTimeStamp+".txt"
+                orderTimeStamp = os.path.join(Directory,orderTimeStamp)
+                with open(orderTimeStamp,"w") as fileHandleToSaveTheBill:
+                    fileHandleToSaveTheBill.write("Staff Bill:\n")
+                    fileHandleToSaveTheBill.write("-"*70)
+                    for key in self._orderDict:
+                        fileHandleToSaveTheBill.write("\n %-20s Qty: %-10d Price: $%-10.2f Total: $%-10.2f" %(key,self._orderDict[key],self._priceDict[key],\
+                                                                                (self._orderDict[key]*self._priceDict[key]))+"\n")
+                        fileHandleToSaveTheBill.write("-"*70)
+                    fileHandleToSaveTheBill.write("\nPrice before tax: " + str(round(self._priceBtax,2)))
+                    fileHandleToSaveTheBill.write("\nPrice after tax: "+str(round(self._priceAtax,2)))
+                    Flag1 = False
+    
+    def Create(self):
+        '''
+        Here we determine if the user wants to create an order
+        '''
+        print("\nWould you like to create an order?")
+        Flag1 = True
+        while Flag1:
+                choice = input("Press 1 for yes, 2 for no: ")
+                if choice == "2":
+                    Flag1 = False
+                elif choice == "1":
+                    Flag1 = False
+                    order = Staff()
+                    order.displayMenu()
+                    order.getInputs()
+                    Flag1 = True
+                    while Flag1:
+                        order.calculate()
+                        choice = input("\nPlease enter R to Read the order, U to update the order, D to Delete within the order, or 0 to pay: ")
+                        if choice == "R":
+                            order.Read()
+                        elif choice == "U":
+                            order.Update()
+                        elif choice == "D":
+                            order.Delete()
+                        elif choice == "0":
+                            Flag1 = False
+                        else:
+                            print("Error: Invalid Selection\n")
+                            print("Please try again (Case Sensitive)")
+                    order.printBill()
+                    order.saveToFile()
+                else:
+                    print("Please enter a valid number\n")
+
+           
---
+++ PersonSubClass/StudentSubClass.py
@@ -0,0 +1,92 @@
+#Jose Alvaro Leos
+#CIS 41A
+#6/2/2020
+#This class contains the Student Subclass - extends Person SuperClass
+
+import time
+import datetime
+import os
+from os import path
+from SuperPerson import Person
+
+class Student(Person):
+    
+    '''
+    This subclass Student is-a Person. This subclass will change the tax rate
+    and a little line of code for the output file
+    '''
+    def __init__(self):
+        '''
+        We override the method from Person since we know this class is a Student,
+        therefore the tax variable is set to 0
+        '''
+        super().__init__()
+        self._tax = 0
+                             
+    def saveToFile(self):
+        '''
+        This part basically copies the printBill function but instead of printing
+        we will be writing to an outfile.It will include an identical copy along with
+        time stamps; small change - output file will state "Student Bill"
+        '''
+        Flag1 = True
+        while Flag1:
+            if self._total == 0:
+                print("\nThank you! Take Care!")
+                Flag1 = False
+            else:
+                Directory = os.getcwd()  
+                if path.isdir("Student Receipts"):
+                    print("Receipt Added to Student Receipt Folder")
+                else:
+                    os.chdir(Directory)
+                    os.mkdir("Student Receipts")
+                Directory = os.path.join(Directory,"Student Receipts")
+                timeStamp = time.time()
+                orderTimeStamp = datetime.datetime.fromtimestamp(timeStamp).strftime('%Y-%m-%d %H-%M-%S')
+                orderTimeStamp = orderTimeStamp+".txt"
+                orderTimeStamp = os.path.join(Directory,orderTimeStamp)
+                with open(orderTimeStamp,'w') as fileHandleToSaveTheBill :
+                    fileHandleToSaveTheBill.write("Student Bill:\n")
+                    fileHandleToSaveTheBill.write("-"*70)
+                    for key in self._orderDict:
+                        fileHandleToSaveTheBill.write("\n %-20s Qty: %-10d Price: $%-10.2f Total: $%-10.2f" %(key,self._orderDict[key],self._priceDict[key],\
+                                                                                (self._orderDict[key]*self._priceDict[key]))+"\n")
+                        fileHandleToSaveTheBill.write("-"*70)
+                    fileHandleToSaveTheBill.write("\nPrice before tax: " + str(round(self._priceBtax,2)))
+                    fileHandleToSaveTheBill.write("\nPrice after tax: "+str(round(self._priceAtax,2)))
+                    Flag1 = False
+    def Create(self):
+        '''
+        Here we determine if the user wants to create an order
+        '''
+        print("\nWould you like to create an order?")
+        Flag1 = True
+        while Flag1:
+                choice = input("Press 1 for yes, 2 for no: ")
+                if choice == "2":
+                    Flag1 = False
+                elif choice == "1":
+                    Flag1 = False
+                    order = Student()
+                    order.displayMenu()
+                    order.getInputs()
+                    Flag1 = True
+                    while Flag1:
+                        order.calculate()
+                        choice = input("\nPlease enter R to Read the order, U to update the order, D to Delete within the order, or 0 to pay: ")
+                        if choice == "R":
+                            order.Read()
+                        elif choice == "U":
+                            order.Update()
+                        elif choice == "D":
+                            order.Delete()
+                        elif choice == "0":
+                            Flag1 = False
+                        else:
+                            print("Error: Invalid Selection\n")
+                            print("Please try again (Case Sensitive)")
+                    order.printBill()
+                    order.saveToFile()
+                else:
+                    print("Please enter a valid number\n")
